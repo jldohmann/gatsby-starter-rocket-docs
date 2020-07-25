5055add53644f41781f8e13b7caf6789f9ed67f4
unknown <jldohmann@utexas.edu>
2020-07-25 18:47:54 -0500

    adding about page and header component

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+        <div style={{ color: `#800000`  }}>
+            <Header />
+            <p>I'm Jesse, and I made this site using Gatsby!</p>
+        </div>
+    )
